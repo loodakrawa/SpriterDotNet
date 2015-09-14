d203ee73c1d1dee14171d1d1e5f9a20fa8c2fac2
Luka Sverko <sverklu@SYDICT064.ozforex.local>
2015-09-14 17:01:43 +1000

    Fix license headers.

--- SpriterDotNet.Unity/Assets/SpriterDotNet/SpriterImporter.cs
+++ SpriterDotNet.Unity/Assets/SpriterDotNet/SpriterImporter.cs
@@ -1,4 +1,9 @@
-﻿#if UNITY_EDITOR
+﻿// Copyright (c) 2015 The original author or authors
+//
+// This software may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+
+#if UNITY_EDITOR
 
 using SpriterDotNet;
 using System;
