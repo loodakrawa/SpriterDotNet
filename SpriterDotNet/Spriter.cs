3a8817f7780d4d3205cd9dbb58aeab3414c2f18a
Luka Sverko <sverklu@SYDICT064.ozforex.local>
2015-10-06 15:52:37 +1100

    Change from MIT to zlib license

--- SpriterDotNet/Spriter.cs
+++ SpriterDotNet/Spriter.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2015 The original author or authors
 //
 // This software may be modified and distributed under the terms
-// of the MIT license.  See the LICENSE file for details.
+// of the zlib license.  See the LICENSE file for details.
 
 using System;
 using System.Xml.Serialization;
