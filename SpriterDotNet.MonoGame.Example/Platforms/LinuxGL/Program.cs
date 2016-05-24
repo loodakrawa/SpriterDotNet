85a2d16e0ca1e96d7f8561f601ca24f4837ace0a
unknown <unknown@unknown.com>
2016-05-24 16:39:23 +1000

    Add linux platform

---
+++ SpriterDotNet.MonoGame.Example/Platforms/LinuxGL/Program.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2015 The original author or authors
+//
+// This software may be modified and distributed under the terms
+// of the zlib license.  See the LICENSE file for details.
+
+using System;
+
+namespace SpriterDotNet.MonoGame.Example.WindowsGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (SpriterGame game = new SpriterGame())
+            {
+                game.Run();
+            }
+        }
+    }
+}
