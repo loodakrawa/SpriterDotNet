88d0a743908f4bc794d8d519b5a2471e39a7c5c1
loodakrawa <loodakrawa@gmail.com>
2016-05-28 13:39:27 +1000

    Add linux platform

---
+++ SpriterDotNet.MonoGame.Example/Platforms/LinuxGL/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("SpriterDotNet.MonoGame.Example.LinuxGL")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: ComVisible (false)]
