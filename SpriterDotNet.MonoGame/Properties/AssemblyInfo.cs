d2df3a676e24516e98a63a89324b1614ae6ba43a
loodakrawa <loodakrawa@gmail.com>
2016-06-11 12:32:07 +1000

    Update for NuGet

--- SpriterDotNet.MonoGame/Properties/AssemblyInfo.cs
+++ SpriterDotNet.MonoGame/Properties/AssemblyInfo.cs
@@ -2,6 +2,7 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("SpriterDotNet.MonoGame")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0")]
+[assembly: AssemblyFileVersion("1.5.0")]
+[assembly: AssemblyInformationalVersion("1.5.0")]
 [assembly: ComVisible(false)]
